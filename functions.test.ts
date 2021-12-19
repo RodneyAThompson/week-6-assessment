de41efb994f35eab5bef47f8d7737374beb36251
Rodney Thompson <godneyson06@gmail.com>
2021-12-19 12:11:04 -0800

    touched it up

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,22 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should return array', () => {
-    // CODE HERE
+const testArr = [1, 2, 3, 4];
+
+describe('shuffleArray should return a random ordered array', () => {
+    test('ShuffleArray returns an array', () => {
+        expect(Array.isArray(shuffleArray(testArr))).toBe(true);
+    });
+
+    test('Should have the same length as the original array', () => {
+        expect(shuffleArray(testArr)).toHaveLength(4);
+    });
+
+    test('All the items are the same as before', () => {
+        expect(shuffleArray(testArr)).toEqual(expect.arrayContaining(testArr));
+    });
+
+    test('Items have been shuffled', () => {
+        let result = shuffleArray(testArr);
+        expect(result.join()).not.toEqual(testArr.join());
+    });
 })
